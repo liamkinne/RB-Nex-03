c36fc8177dca0be425ff6d1d05724bf22f201191
Liam Kinne <kinneliam@gmail.com>
2018-08-14 17:23:01 +1000

    add correction to main logic

--- RB-Nex-03.ino
+++ RB-Nex-03.ino
@@ -1,5 +1,6 @@
 #include "L298.h"
 #include "MechanumDrive.h"
+#include "AnalogGyro.h"
 
 L298 motor_a(9, 8);
 L298 motor_b(5, 4);
@@ -8,19 +9,25 @@ L298 motor_d(10, 11);
 
 MechanumDrive mech(motor_a, motor_b, motor_c, motor_d);
 
-int counter = 0;
+AnalogGyro gyro(A0);
 
+int counter = 0;
 
 void setup()
 {
-	TCCR0B = TCCR0B & 0b11111000 | 0x01;
+	TCCR0B = TCCR0B & 0b11111000 | 0x01; // multiply delays by 64 to compensate
 	TCCR1B = TCCR1B & 0b11111000 | 0x01;
 	TCCR2B = TCCR2B & 0b11111000 | 0x01;
+	delay(100);
+	gyro.reset_angle();
 }
 
 void loop()
 {
-	mech.set(sin(counter / 100.0) * 35.0 , cos(counter / 100.0) * 35.0 , 0.0);
+	delay(SONAR::duration);
+	mech.set(sin(counter / 100.0) * 35.0 , cos(counter / 100.0) * 35.0 , -gyro.get_angle() / 90);
 	delay(400);
 	counter++;
+	gyro.update();
+	delay(500);
 }
